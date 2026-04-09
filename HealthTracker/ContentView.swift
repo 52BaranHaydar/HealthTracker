d9ecb1c94778492ec2c6606be8463e00969a974d
Baran Haydar <baranhaydar80@gmail.com>
2026-04-09 21:33:54 +0300

    Initial Commit

---
+++ HealthTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthTracker
+//
+//  Created by Baran on 9.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
